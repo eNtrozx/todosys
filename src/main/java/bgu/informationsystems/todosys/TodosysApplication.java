e40bbe0d9da9b89635ecaa9d0a12df59c7022cb8
eNtrozx <64536445+eNtrozx@users.noreply.github.com>
2022-03-30 21:23:13 +0300

    init spring

---
+++ src/main/java/bgu/informationsystems/todosys/TodosysApplication.java
@@ -0,0 +1,13 @@
+package bgu.informationsystems.todosys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodosysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodosysApplication.class, args);
+	}
+
+}
